961be5cacc79344c3e3951fbab707ff9a4821ec6
Cristian <cristianf@me.com>
2016-07-09 20:06:38 -0400

    Complete :)

--- kalman.py
+++ kalman.py
@@ -2,63 +2,91 @@ import matplotlib
 import matplotlib.cm as cm
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
-
-
-
 import numpy as np
-EXTERNAL_INFLUENCE = False
+from numpy.linalg import inv
 
+# For our [positon, velocity] example, this would be applied acceelration
+EXTERNAL_INFLUENCE = True
+DISPLAY = True  # Want to see a graph of the estiamte?
 
-Xk = np.matrix([0.0, 1.0]).T  # state vector [position, velocity]
-Pk = np.matrix([[1, 0.5], [1, 0.5]])  # covariance matrix
+Xk = np.matrix([1.0, 0.5]).T  # state vector [position, velocity]
+Pk = np.matrix([[1.0, 0.0], [0.0, 1.0]])  # covariance matrix
 delta_t = 1
 
+
 for i in range(5):
-	# ------- Prediction -----------
-	Fk = np.matrix([[1, delta_t], [0, 1]])  # prediction step
-	# 1,1  So this means on each iteration, the position
-	# 0,1  will increase by the velocity * delta_t
+    # ------- Prediction -----------
+    Fk = np.matrix([[1.0, delta_t], [0.0, 1.0]])  # prediction step
+    # 1,dT  So this means on each iteration, the position
+    # 0,1  will increase by the velocity * delta_t
 
-	if EXTERNAL_INFLUENCE:
-	    uk = [0, 1]  # [acceleration formula for position (pk),
-	    # acceleration formula for velocity(vk)]
-	    # when [0, 0] it becomes the same as if there was not external_influnece
-	    Bk = np.matrix([delta_t**2 / 2, delta_t])  # control matrix,
-	    # with the equations of motions regarding time change
-	    # additional uncertainty from the environment matrix
-	    Qk = np.matrix([[1.0, 0.05], [0.05, 1.0]])
-	    # Mean of prediction, given external influence
-	    Xk = np.dot(Fk, Xk) + np.dot(Bk, uk)
-	    Pk = np.dot(np.dot(Fk, Pk), Fk.T) + Qk
-	else:
-	    Xk = np.dot(Fk, Xk)  # The mean of the prediction
-	    Pk = np.dot(np.dot(Fk, Pk), Fk.T)  # The covariance matrix of the preiction
+    if EXTERNAL_INFLUENCE:
+        uk = [0, 0.25]  # [acceleration formula for position (pk),
+        # acceleration formula for velocity(vk)]
+        # when [0, 0] it becomes the same as if there was not
+        # external_influnece
+        Bk = np.matrix([delta_t**2 / 2, delta_t])  # control matrix,
+        # with the equations of motions regarding time change
+        # additional uncertainty from the environment matrix
+        Qk = np.matrix([[1.0, 0.01], [0.01, 1.]])
+        # Mean of prediction, given external influence
+        Xk = np.dot(Fk, Xk) + np.dot(Bk, uk)
+        Pk = np.dot(np.dot(Fk, Pk), Fk.T) + Qk
+    else:
+        Xk = np.dot(Fk, Xk)  # The mean of the prediction
+        # The covariance matrix of the preiction
+        Pk = np.dot(np.dot(Fk, Pk), Fk.T)
+    # -------- /Prediction ---------
 
-	# -------- /Prediction ---------
+    # -------- Measurments ---------
+    Hk = np.matrix([[1.0, 0.0], [0.0, 1.0]])  # maps the sensor reading values to
+    # the state vector values
+    # mean of the expected measurment based on the predicted step
+    # brings the values from the prediction to the values of the sensor
+    Xk = np.dot(Hk, Xk)
+    # covariance of the measurment based on the predicted step
+    # Brings the values of the prediction to the values of sensor
+    Pk = np.dot(np.dot(Hk, Pk), Hk.T)
 
+    # #Sensor Values
+    Zk = np.mean([i**2, 1.5])  # Mean value of sensors
+    # # Covariance matrix of measurments
+    Rk = np.matrix([[1.0, 0.0], [0.0, 1.0]])
+    # -------- /Measurments ---------
 
+    # # -------- Update Step ---------
+    intuition = np.dot(np.dot(Hk, Pk), Hk.T) + Rk
+    # # print intuition
 
-	print "Xk:","\n",Xk
-	print "Pk:","\n", Pk
-	# print Pk[0,0]
-	# print Pk[1,1]
-	# print Pk[0,1]
-	# print Xk[0,0]
-	# print Xk[1,0]
+    K = np.dot(np.dot(Pk, Hk.T), inv(intuition))
+    Xk = Xk + np.dot(K, (Zk - np.dot(Hk, Xk)))
+    Pk = Pk - np.dot(K, np.dot(Hk, Pk))
 
-	
-	# -------- Display ---------
-	delta = 0.025
-	x = np.arange(0.0, 50.0, delta)
-	y = np.arange(0.0, 10.0, delta)
-	X, Y = np.meshgrid(x, y)
-	Z1 = mlab.bivariate_normal(X, Y, Pk[0,0], Pk[1,1], Xk[0,0], Xk[1,0], Pk[0,1])
-	Z = 10.0 * Z1
+    # -------- /Update Step ---------
+    print "i:",i
+    print "Zk:",Zk
+    print "Xk:", "\n", Xk
+    print "Pk:", "\n", Pk
+    # print Pk[0,0]
+    # print Pk[1,1]
+    # print Pk[0,1]
+    # print Xk[0,0]
+    # print Xk[1,0]
 
+    # -------- Display ---------
+    if DISPLAY:
+        delta = 1.25
+        x = np.arange(-3.0, 50.0, delta)
+        y = np.arange(-3.0, 50.0, delta)
+        X, Y = np.meshgrid(x, y)
+        Z1 = mlab.bivariate_normal(X, Y, Pk[0, 0], Pk[1, 1], Xk[
+                                   0, 0], Xk[1, 0], Pk[0, 1])
+        Z = 10.0 * Z1
 
-	plt.figure()
-	CS = plt.contour(X, Y, Z)
-	plt.clabel(CS, inline=1, fontsize=10)
+        plt.figure()
+        CS = plt.contour(X, Y, Z)
+        # CS = plt.pcolormesh(X, Y, Z)
+        # plt.clabel(CS, inline=0, fontsize=10)
 
-	plt.show()
-	# -------- /Display ---------
+        plt.show()
+    # -------- /Display ---------
