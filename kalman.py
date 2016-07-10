f48afc6dd371e620f664fd6e997295473b6cb2fb
Cristian <cristianf@me.com>
2016-07-10 11:01:59 -0400

    Added cumulative error calculation

--- kalman.py
+++ kalman.py
@@ -7,14 +7,16 @@ from numpy.linalg import inv
 
 # For our [positon, velocity] example, this would be applied acceelration
 EXTERNAL_INFLUENCE = True
-DISPLAY = True  # Want to see a graph of the estiamte?
+DISPLAY = False  # Want to see a graph of the estiamte?
 
 Xk = np.matrix([1.0, 0.5]).T  # state vector [position, velocity]
-Pk = np.matrix([[1.0, 0.0], [0.0, 1.0]])  # covariance matrix
+Pk = np.matrix([[1.0, 0.25], [0.25, 1.0]])  # covariance matrix
 delta_t = 1
 
+ErrorBetweenSensor=0
+ErrorBetweenPrediction=0
 
-for i in range(5):
+for i in range(1,1000):
     # ------- Prediction -----------
     Fk = np.matrix([[1.0, delta_t], [0.0, 1.0]])  # prediction step
     # 1,dT  So this means on each iteration, the position
@@ -28,7 +30,7 @@ for i in range(5):
         Bk = np.matrix([delta_t**2 / 2, delta_t])  # control matrix,
         # with the equations of motions regarding time change
         # additional uncertainty from the environment matrix
-        Qk = np.matrix([[1.0, 0.01], [0.01, 1.]])
+        Qk = np.matrix([[1.0, 0.01], [0.01, 1.0]])
         # Mean of prediction, given external influence
         Xk = np.dot(Fk, Xk) + np.dot(Bk, uk)
         Pk = np.dot(np.dot(Fk, Pk), Fk.T) + Qk
@@ -39,7 +41,7 @@ for i in range(5):
     # -------- /Prediction ---------
 
     # -------- Measurments ---------
-    Hk = np.matrix([[1.0, 0.0], [0.0, 1.0]])  # maps the sensor reading values to
+    Hk = np.matrix([[1.0, 0.5], [0.5, 1.0]])  # maps the sensor reading values to
     # the state vector values
     # mean of the expected measurment based on the predicted step
     # brings the values from the prediction to the values of the sensor
@@ -49,24 +51,32 @@ for i in range(5):
     Pk = np.dot(np.dot(Hk, Pk), Hk.T)
 
     # #Sensor Values
-    Zk = np.mean([i**2, 1.5])  # Mean value of sensors
+    Sensor_reading = np.random.normal(i,i*0.25)
+    Zk = np.mean([Sensor_reading, Sensor_reading])  # Mean value of sensors
     # # Covariance matrix of measurments
-    Rk = np.matrix([[1.0, 0.0], [0.0, 1.0]])
+    Rk = np.matrix([[1.0, 0.25], [0.25, 1.0]])
     # -------- /Measurments ---------
 
-    # # -------- Update Step ---------
-    intuition = np.dot(np.dot(Hk, Pk), Hk.T) + Rk
-    # # print intuition
 
-    K = np.dot(np.dot(Pk, Hk.T), inv(intuition))
+    print "Xk_pre:", Xk[0,0]
+
+    # # -------- Update Step ---------
+    inovation = np.dot(np.dot(Hk, Pk), Hk.T) + Rk
+    # # print inovation
+
+    K = np.dot(np.dot(Pk, Hk.T), inv(inovation))
     Xk = Xk + np.dot(K, (Zk - np.dot(Hk, Xk)))
     Pk = Pk - np.dot(K, np.dot(Hk, Pk))
 
     # -------- /Update Step ---------
-    print "i:",i
-    print "Zk:",Zk
-    print "Xk:", "\n", Xk
-    print "Pk:", "\n", Pk
+    print "Sensor:",Sensor_reading
+    print "Real Value:",i
+    # print "Zk:",Zk
+    print "Xk_post:", Xk[0,0]
+    print "--------"
+    ErrorBetweenPrediction += np.abs(Xk[0,0]-i)
+    ErrorBetweenSensor += np.abs(Sensor_reading-i)
+    # print "Pk:", "\n", Pk
     # print Pk[0,0]
     # print Pk[1,1]
     # print Pk[0,1]
@@ -75,9 +85,9 @@ for i in range(5):
 
     # -------- Display ---------
     if DISPLAY:
-        delta = 1.25
-        x = np.arange(-3.0, 50.0, delta)
-        y = np.arange(-3.0, 50.0, delta)
+        delta = 0.25
+        x = np.arange(-3.0, 10.0, delta)
+        y = np.arange(-3.0, 10.0, delta)
         X, Y = np.meshgrid(x, y)
         Z1 = mlab.bivariate_normal(X, Y, Pk[0, 0], Pk[1, 1], Xk[
                                    0, 0], Xk[1, 0], Pk[0, 1])
@@ -90,3 +100,5 @@ for i in range(5):
 
         plt.show()
     # -------- /Display ---------
+print "Predict Error:",ErrorBetweenPrediction
+print "Sensor Error:",ErrorBetweenSensor
